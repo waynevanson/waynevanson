cba2034871dcccc1f3c5cc3c7532fa575ff8c719
Wayne Van Son <waynevanson@gmail.com>
2024-04-18 22:17:04 +1000

    fix: correct the schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,12 +3,9 @@ import { defineCollection, z } from "astro:content"
 export const collections = {
   posts: defineCollection({
     type: "content",
-    schema: z.array(
-      z.object({
-        title: z.string(),
-        summary: z.string(),
-        body: z.string(),
-      })
-    ),
+    schema: z.object({
+      title: z.string(),
+      summary: z.string(),
+    }),
   }),
 }
